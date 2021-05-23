9c4b1ebc15d80d68ce53a5e7de320730ca9907ef
venkatesh123973 <venkatesh.mepco@gmail.com>
2021-05-24 00:01:08 +0530

    Initial

---
+++ FirstProject/src/main/java/GITCode/meet.java
@@ -0,0 +1,14 @@
+package GITCode;
+
+import org.testng.annotations.Test;
+
+public class meet {
+	public class transfer {
+
+		@Test
+		public void code() {
+			System.out.println("Welcome to Github");
+			
+		}
+	}
+}
---
+++ FirstProject/src/test/java/hubcode/transfer.java
@@ -0,0 +1,12 @@
+package hubcode;
+
+import org.testng.annotations.Test;
+
+public class transfer {
+
+	@Test
+	public void code() {
+		System.out.println("Welcome to Github");
+		
+	}
+}
